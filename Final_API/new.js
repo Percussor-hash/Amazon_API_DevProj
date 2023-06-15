44ff52dc6e554329e5ecaf242f4b4e81fdcb2440
Percussor-hash <85029612+Percussor-hash@users.noreply.github.com>
2023-06-15 11:19:36 +0530

    Update new.js

--- Final_API/new.js
+++ Final_API/new.js
@@ -4,176 +4,150 @@ require('dotenv').config();
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
+const isValidSearchQuery = require('./utils/validation');
+
 const app = express();
 const PORT = process.env.PORT;
+
 app.use(bodyParser.json());
+app.use(cors());
 
-const MAX_RETRIES = 3; // Maximum number of retry attempts
-const RETRY_DELAY = 10000; // Delay between retries in milliseconds
+const MAX_RETRIES = 3;
+const RETRY_DELAY = 10000;
 
-let allProducts = []; // Array to store products from all pages
+let allProducts = [];
 
-app.use(cors()); // Add parentheses to invoke the cors middleware
-
-//const MAX_RETRIES = 3; // Maximum number of retry attempts
-
-
-
-function isValidSearchQuery(query) {
-  // Remove leading and trailing whitespace
-  query = query.replace(/\s/g, '');
-
-  // Check if the query is empty
-  if (query.length === 0) {
-    return false;
-  }
-
-  // Add additional validation criteria as per your requirements
-  if (!/^[a-zA-Z0-9-_]+$/.test(query)) {
-    return false;
-  }
-
-  // Return true if the query passes all validation checks
-  return true;
-}
-
-
-//GET
+// GET endpoint for search
 app.get('/search', async (req, res) => {
+  allProducts = []; // Clear allProducts array
+  const { title, maxPages = 20 } = req.query;
+
+  if (!isValidSearchQuery(title)) {
+    return res.status(400).send('Invalid search query.');
+  }
+
   let retryCount = 0;
   let currentPage = 1;
-
-  const searchQuery = req.query.title;
-  const pages = req.query.maxPages;
-  if (!isValidSearchQuery(searchQuery)) {
-    console.log("Invalid search query.");
-    res.send("Invalid Input query or no.of pages,\n Try again.")
-  } else {
-    console.log("Search query is valid.");
-
-    const maxPages = parseInt(pages) || 20;
-   // Maximum number of pages to scrape, defaulting to 20 if not provided
-  
 
   while (currentPage <= maxPages) {
     try {
       const browser = await puppeteer.launch();
       const page = await browser.newPage();
 
+      await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3');
       await page.goto('https://www.amazon.com/');
-      await page.type('#twotabsearchtextbox', req.query.title);
+      await page.type('#twotabsearchtextbox', title);
       await page.click('#nav-search-submit-button');
       await page.waitForNavigation();
 
       console.log(`Scraping page ${currentPage}...`);
 
-      const products = await page.evaluate(() => {
-        const results = [];
-        const items = document.querySelectorAll('.s-result-item .s-card-border');
-        items.forEach((item) => {
-            const title = item.querySelector('h2 > a > span');
-            const price = item.querySelector('.a-price-whole');
-            const cents = item.querySelector('.a-price-fraction');
-            const image = item.querySelector('img');
-          if (title && price && image) {
-            const formattedPrice = `${parseInt(price.innerText)}.${parseInt(cents.innerText)}`;
+      const productsOnPage = await page.evaluate(() => {
+        const products = [];
+        const productCards = document.querySelectorAll('.s-result-item .s-card-border');
+        productCards.forEach((card) => {
+          const name = card.querySelector('h2 > a > span');
+          const wholePrice = card.querySelector('.a-price-whole');
+          const fractionPrice = card.querySelector('.a-price-fraction');
+          const image = card.querySelector('img');
+          if (name && wholePrice && image) {
+            const formattedPrice = `${parseInt(wholePrice.innerText)}.${parseInt(fractionPrice.innerText)}`;
             const product = {
-              title: title.innerText,
+              name: name.innerText,
               price: parseFloat(formattedPrice),
               image: image.getAttribute('src')
             };
-            results.push(product);
+            products.push(product);
           }
         });
-        return results;
+        return products;
       });
 
-      allProducts = allProducts.concat(products);
+      allProducts = allProducts.concat(productsOnPage);
 
       await browser.close();
 
       currentPage++;
     } catch (error) {
+      console.error(`Error occurred. Retrying (${retryCount}/${MAX_RETRIES})...`);
       retryCount++;
-      console.log(`Error occurred. Retrying (${retryCount}/${maxPages})...`);
-      await delay(RETRY_DELAY); // Wait before retrying
+      await new Promise(resolve => setTimeout(resolve, RETRY_DELAY));
     }
   }
-  console.log(`Scraped all pages requested`);
+
+  console.log(`Scraped ${allProducts.length} product(s)`);
   res.json(allProducts);
-}});
+});
 
-
-//POST
+// POST endpoint for search
 app.post('/search', async (req, res) => {
+  allProducts = []; // Clear allProducts array
+  const { title, maxPages = 20 } = req.body;
+
+  if (!isValidSearchQuery(title)) {
+    return res.status(400).send('Invalid search query.');
+  }
+
   let retryCount = 0;
   let currentPage = 1;
 
-  const searchQuery = req.body.title;
-  const pages = req.body.maxPages;
-  if (!isValidSearchQuery(searchQuery)) {
-    console.log("Invalid search query.");
-    res.send("Invalid Input query or no.of pages,\n Try again.")
-  } else {
-    console.log("Search query is valid.");
-  
-
-    const maxPages = parseInt(pages, 10) || 20;
-    // Maximum number of pages to scrape, defaulting to 20 if not provided
-
-  
   while (currentPage <= maxPages) {
     try {
       const browser = await puppeteer.launch();
       const page = await browser.newPage();
 
+      await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3');
       await page.goto('https://www.amazon.com/');
-      await page.type('#twotabsearchtextbox', req.body.title);
+      await page.type('#twotabsearchtextbox', title);
       await page.click('#nav-search-submit-button');
       await page.waitForNavigation();
 
       console.log(`Scraping page ${currentPage}...`);
 
-      const products = await page.evaluate(() => {
-        const results = [];
-        const items = document.querySelectorAll('.s-result-item .s-card-border');
-        items.forEach((item) => {
-            const title = item.querySelector('h2 > a > span');
-            const price = item.querySelector('.a-price-whole');
-            const cents = item.querySelector('.a-price-fraction');
-            const image = item.querySelector('img');
-          if (title && price && image) {
-            const formattedPrice = `${parseInt(price.innerText)}.${parseInt(cents.innerText)}`;
+      const productsOnPage = await page.evaluate(() => {
+        const products = [];
+        const productCards = document.querySelectorAll('.s-result-item .s-card-border');
+        productCards.forEach((card) => {
+          const name = card.querySelector('h2 > a > span');
+          const wholePrice = card.querySelector('.a-price-whole');
+          const fractionPrice = card.querySelector('.a-price-fraction');
+          const image = card.querySelector('img');
+          if (name && wholePrice && image) {
+            const formattedPrice = `${parseInt(wholePrice.innerText)}.${parseInt(fractionPrice.innerText)}`;
             const product = {
-              title: title.innerText,
+              name: name.innerText,
               price: parseFloat(formattedPrice),
               image: image.getAttribute('src')
             };
-            results.push(product);
+            products.push(product);
           }
         });
-        return results;
+        return products;
       });
 
-      allProducts = allProducts.concat(products);
-      
+      allProducts = allProducts.concat(productsOnPage);
+
       await browser.close();
 
       currentPage++;
     } catch (error) {
+      console.error(`Error occurred. Retrying (${retryCount}/${MAX_RETRIES})...`);
       retryCount++;
-      console.log(`Error occurred. Retrying (${retryCount}/${maxPages})...`);
-      await delay(RETRY_DELAY); // Wait before retrying
+      await new Promise(resolve => setTimeout(resolve, RETRY_DELAY));
     }
   }
-  console.log(`Scraped all pages requested`);
+
+  console.log(`Scraped ${allProducts.length} product(s)`);
   res.json(allProducts);
-}});
+});
 
-function delay(ms) {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-}
+// DELETE endpoint to clear search results
+app.delete('/search', (req, res) => {
+  allProducts = []; // Clear allProducts array
+  res.send('Search results cleared.');
+});
 
+// Start the server
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server listening on port ${PORT}.`);
 });
